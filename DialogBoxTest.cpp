d5b1e0f4f89b975489ff7b5b0123c96cf5946f83
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2020-06-09 21:15:43 +0900

    first commit

---
+++ DialogBoxTest.cpp
@@ -0,0 +1,45 @@
+#include <windows.h>
+#include <windowsx.h>
+#include <commctrl.h>
+
+BOOL OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
+{
+    return TRUE;
+}
+
+void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
+{
+    switch (id)
+    {
+    case IDOK:
+        MessageBox(NULL, TEXT("OK button clicked"), TEXT("DialogBoxTest"), MB_ICONINFORMATION);
+        EndDialog(hwnd, id);
+        break;
+    case IDCANCEL:
+        MessageBox(NULL, TEXT("Cancel button clicked"), TEXT("DialogBoxTest"), MB_ICONINFORMATION);
+        EndDialog(hwnd, id);
+        break;
+    }
+}
+
+INT_PTR CALLBACK
+DialogProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        HANDLE_MSG(hwnd, WM_INITDIALOG, OnInitDialog);
+        HANDLE_MSG(hwnd, WM_COMMAND, OnCommand);
+    }
+    return 0;
+}
+
+INT WINAPI
+WinMain(HINSTANCE   hInstance,
+        HINSTANCE   hPrevInstance,
+        LPSTR       lpCmdLine,
+        INT         nCmdShow)
+{
+    InitCommonControls();
+    DialogBox(hInstance, MAKEINTRESOURCE(1), NULL, DialogProc);
+    return 0;
+}
